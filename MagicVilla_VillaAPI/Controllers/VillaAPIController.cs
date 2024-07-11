f088b7e7f3d795086c2ea3965c9bd53d5c9f9d07
Mushfique <mushfique3214@gmail.com>
2024-07-11 20:47:51 +0600

    DTOS

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,26 @@
+﻿using MagicVilla_VillaAPI.DataStore;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villas;
+        }
+
+        [HttpGet("id")]
+
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villas.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla_VillaAPI.Models.DTOs;
+
+namespace MagicVilla_VillaAPI.DataStore
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villas = new List<VillaDTO> { { new VillaDTO { Id = 1, Name = "Pool View" } } };
+    }
+}
